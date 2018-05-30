7db127c599878b4ecc5c874f945fd75831899c67
Marc Raftesath (Manpower SA (Pty) Ltd) <Marcraffy@gmail.com>
2018-05-30 10:34:01 +0200

    cleanStarterCode

--- StarterBot/Bot.cs
+++ StarterBot/Bot.cs
@@ -8,149 +8,88 @@ namespace StarterBot
 {
     public class Bot
     {
-        private readonly GameState _gameState;
+        private readonly GameState gameState;
 
-        private readonly BuildingStats _attackStats;
-        private readonly BuildingStats _defenseStats;
-        private readonly BuildingStats _energyStats;
+        private readonly BuildingStats attackStats;
+        private readonly BuildingStats defenseStats;
+        private readonly BuildingStats energyStats;
 
-        private readonly int _mapWidth;
-        private readonly int _mapHeight;
-        private readonly Player _player;
-        private readonly Random _random;
+        private readonly int mapWidth;
+        private readonly int mapHeight;
+        private readonly Player player;
+        private readonly Random random;
 
         public Bot(GameState gameState)
         {
-            this._gameState = gameState;
-            this._mapHeight = gameState.GameDetails.MapHeight;
-            this._mapWidth = gameState.GameDetails.MapWidth;
+            this.gameState = gameState;
+            mapHeight = gameState.GameDetails.MapHeight;
+            mapWidth = gameState.GameDetails.MapWidth;
 
-            this._attackStats = gameState.GameDetails.BuildingsStats[BuildingType.Attack];
-            this._defenseStats = gameState.GameDetails.BuildingsStats[BuildingType.Defense];
-            this._energyStats = gameState.GameDetails.BuildingsStats[BuildingType.Energy];
+            attackStats = gameState.GameDetails.BuildingsStats[BuildingType.Attack];
+            defenseStats = gameState.GameDetails.BuildingsStats[BuildingType.Defense];
+            energyStats = gameState.GameDetails.BuildingsStats[BuildingType.Energy];
 
-            this._random = new Random((int) DateTime.Now.Ticks);
+            random = new Random((int) DateTime.Now.Ticks);
 
-            _player = gameState.Players.Single(x => x.PlayerType == PlayerType.A);
+            player = gameState.Players.Single(x => x.PlayerType == PlayerType.A);
         }
 
         public string Run()
         {
-            var commandToReturn = "";
-
-            //This will check if there is enough energy to build any building before processing any commands
-            if (_player.Energy < _defenseStats.Price || _player.Energy < _energyStats.Price || _player.Energy < _attackStats.Price)
+            if (player.Energy < defenseStats.Price || player.Energy < energyStats.Price || player.Energy < attackStats.Price)
             {
-                return commandToReturn;
+                return "";
             }
-
-            //Get all opponent attack buildings
+            
             var opponentAttackBuildings = GetBuildings(PlayerType.B, BuildingType.Attack);
 
-            //Get all my attack buildings
             var myAttackBuildings = GetBuildings(PlayerType.A, BuildingType.Attack);
-            //Get all my defense buildings
             var myDefenseBuildings = GetBuildings(PlayerType.A, BuildingType.Defense);
 
-            var myBuildings = new List<CellStateContainer>();
-
-            //Combine all buildings into a single list
-            myBuildings.AddRange(myAttackBuildings);
-            myBuildings.AddRange(myDefenseBuildings);
-
-            //Do a random command if enemy has no attack buildings to defend against
-            if (!opponentAttackBuildings.Any())
-            {
-                commandToReturn = GetRandomCommand(myBuildings);
-            }
-            else
-            {
-                //Get all rows with enemy buildings where I don't have a defense building
-                var rows = GetEnemyBuildingRows(opponentAttackBuildings, myDefenseBuildings);
-
-                //Place defense building randomly in first row from list
-                if (rows.Count > 0)
-                {
-                    commandToReturn = GetValidAttackCommand(rows[0], myBuildings);
-                }
-                else
-                {
-                    commandToReturn = GetRandomCommand(myBuildings);
-                }
-            }
-
-            return commandToReturn;
+            var myBuildings = myAttackBuildings.Concat(myDefenseBuildings).ToList(); 
+            
+            var rows = GetUndefendedEnemyBuildingRows(opponentAttackBuildings, myDefenseBuildings);
+            return !opponentAttackBuildings.Any() ? GetRandomCommand(myBuildings) : (rows.Count > 0 ? GetValidAttackCommand(rows[0], myBuildings) : GetRandomCommand(myBuildings));
         }
 
-        //Get a valid attack command for a row
-        public string GetValidAttackCommand(int yCoordinate, List<CellStateContainer> myBuildings)
+        private string GetValidAttackCommand(int yCoordinate, List<CellStateContainer> myBuildings)
         {
-            var xRandom = _random.Next(_mapWidth / 2);
+            var xRandom = random.Next(mapWidth / 2);
 
             while (myBuildings.Any(x => x.X == xRandom && x.Y == yCoordinate && x.Buildings.Any()))
             {
-                xRandom = _random.Next(_mapWidth / 2);
+                xRandom = random.Next(mapWidth / 2);
             }
 
             return $"{xRandom},{yCoordinate},{(int)BuildingType.Defense}";
         }
 
-        //Get random valid command
-        public string GetRandomCommand(List<CellStateContainer> myBuildings)
+        private string GetRandomCommand(List<CellStateContainer> myBuildings)
         {
-            //Place building randomly on my half of the map
-            var xRandom = _random.Next(_mapWidth / 2);
-            var yRandom = _random.Next(_mapHeight);
-            var btRandom = _random.Next(Enum.GetNames(typeof(BuildingType)).Length);
+            var xRandom = random.Next(mapWidth / 2);
+            var yRandom = random.Next(mapHeight);
+            var btRandom = random.Next(Enum.GetNames(typeof(BuildingType)).Length);
 
             while (myBuildings.Any(x => x.X == xRandom && x.Y == yRandom && x.Buildings.Any()))
             {
-                xRandom = _random.Next(_mapWidth / 2);
-                yRandom = _random.Next(_mapHeight);
+                xRandom = random.Next(mapWidth / 2);
+                yRandom = random.Next(mapHeight);
             }
 
             return $"{xRandom},{yRandom},{btRandom}";
         }
-
-        //Get all rows which contain an enemy building where I don't have a defense building
-        private List<int> GetEnemyBuildingRows(List<CellStateContainer> opponentAttackBuildings,
+        
+        private List<int> GetUndefendedEnemyBuildingRows(List<CellStateContainer> opponentAttackBuildings,
             List<CellStateContainer> myDefenseBuildings)
         {
-            var rows = new List<int>();
-            foreach (var enemyAttackBuilding in opponentAttackBuildings)
-            {
-                if (rows.Contains(enemyAttackBuilding.Y)) continue;
-                rows.Add(enemyAttackBuilding.Y);
-            }
-
-            foreach (var myDefenseBuilding in myDefenseBuildings)
-            {
-                if (rows.Contains(myDefenseBuilding.Y))
-                {
-                    rows.Remove(myDefenseBuilding.Y);
-                }
-            }
-
-            return rows;
+            return opponentAttackBuildings.Select(enemyAttackBuilding => enemyAttackBuilding.Y).Where(y => !myDefenseBuildings.Any(myDefenseBuilding => myDefenseBuilding.Y == y)).ToList();
         }
-
-        //Get buildings of a certain type for a certain player
-        private List<CellStateContainer> GetBuildings(PlayerType playerType, BuildingType buildingType)
+        
+        private List<CellStateContainer> GetBuildings(PlayerType playerType, BuildingType buildingType) 
         {
-            var list = new List<CellStateContainer>();
-            foreach (var cellStateContainers in _gameState.GameMap)
-            {
-                foreach (var cellStateContainer in cellStateContainers)
-                {
-                    if (cellStateContainer.CellOwner == playerType &&
-                        cellStateContainer.Buildings.Any(x => x.BuildingType == buildingType))
-                    {
-                        list.Add(cellStateContainer);
-                    }
-                }
-            }
-
-            return list;
+            return gameState.GameMap.SelectMany(cellStateContainers => 
+                cellStateContainers.Where(cellStateContainer => 
+                    cellStateContainer.CellOwner == playerType && cellStateContainer.Buildings.Any(x => x.BuildingType == buildingType))).ToList();
         }
     }
 }
